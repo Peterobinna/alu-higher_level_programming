c6dac18b4b9e54a5545d8e95ecdab16da7e91197
Peter <p.nnamchukw@gmail.com>
2024-07-12 07:02:34 -0700

    updated python more classes

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a simple Rectangle class.
+
+The Rectangle class is currently empty and serves as a placeholder.
+
+"""
+
+
+class Rectangle:
+    """
+    Empty class representing a rectangle.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the Rectangle class.
+        """
+        pass
